e72f7657391cd4b82669b2cdc6d4f01d1cd186eb
dgunay <devingunay@gmail.com>
2018-01-28 13:17:36 -0800

    Install PHPUnit as a development dependency

--- read_chords.php
+++ read_chords.php
@@ -7,7 +7,6 @@
 
 include(__DIR__ . '/vendor/autoload.php');
 
-use ChordChangesReader\Symbol;
 use ChordChangesReader\Parser;
 
 if ($argc <= 1) {
@@ -19,54 +18,3 @@ $text = file_get_contents($argv[1]);
 
 $parser = new Parser();
 $parser->parse($text);
-
-// // TODO: use a stream implementation instead
-// // TODO: maybe sanitize newlines
-// $lines = explode("\n", $text);
-
-// $measures = array();
-// foreach ($lines as $line) {
-// 	// Tokenize the measure
-
-
-// 	$measures = array_merge(
-// 		$measures, 
-// 		array_values(
-// 			array_filter(
-// 				explode('|', $line),
-// 				function($token) {
-// 					return !ctype_space($token);
-// 				}
-// 			)
-// 		)
-// 	);
-// }
-
-// // TODO: use a Progression object to track chords and measures.
-// $piece = array();
-// $measure_number = 1;
-// foreach ($measures as $measure) {
-// 	// TODO: meaningful time signatures
-// 	$piece[$measure_number] = array();
-
-// 	$measure = trim($measure);
-
-// 	// tokenize the measure as chord symbols
-// 	$chords = explode(' ', $measure);
-
-// 	foreach ($chords as $chord_symbol) {
-// 		try {
-// 			$chord = new Chord($chord_symbol);
-// 			$piece[$measure_number][] = $chord;
-// 		}
-// 		catch (\UnexpectedValueException $e) {
-// 			// don't add it to the progression
-// 			// TODO: scream at the user for passing invalid syntax
-// 		}
-// 	}
-
-// 	$measure_number++;
-// }
-
-// // TODO: convert the piece into VexTab notation.
-// print_r($piece);
